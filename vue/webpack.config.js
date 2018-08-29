475032922046007cb4db82237281278b0a465136
arashethis <arashethis@outlook.com>
2018-08-29 16:13:08 +0800

    feat: add vue demo

---
+++ vue/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  mode: 'none',
+  context: path.resolve(__dirname),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          {
+            loader: 'vue-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Vue Example',
+    }),
+    new VueLoaderPlugin()
+  ]
+}
