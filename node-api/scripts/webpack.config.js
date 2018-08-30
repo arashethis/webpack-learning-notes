df664448baca4b417a93c0d56a25d25262537995
arashethis <arashethis@outlook.com>
2018-08-30 16:41:41 +0800

    feat: add node-api demo

---
+++ node-api/scripts/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+  context: path.resolve(__dirname, '..'),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '..', 'dist'),
+    filename: '[name].js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack DevServer Example',
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+}
