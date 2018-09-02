a150ae4b4b02a169b839d4f40a583ef696093a9c
arashethis <arashethis@outlook.com>
2018-09-03 01:06:11 +0800

    feat: add dll-plugin demo

---
+++ dll-plugin/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+  context: path.resolve(__dirname),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack DllPlugin Example',
+      template: 'index.html',
+    }),
+    new webpack.DllReferencePlugin({
+      manifest: require('./dist/vendor.dll.manifest.json')
+    })
+  ]
+}
