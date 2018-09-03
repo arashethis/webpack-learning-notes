9e16b355e0b12d27c271f494574b567f65e3950e
arashethis <arashethis@outlook.com>
2018-09-04 01:08:53 +0800

    feat: add dynamic-import demo

---
+++ dynamic-import/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+  context: path.resolve(__dirname),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack DynamicImport Example',
+    })
+  ]
+}
