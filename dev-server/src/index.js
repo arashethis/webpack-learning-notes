543a877b943aef2e37344953eca5480fe00d16dc
arashethis <arashethis@outlook.com>
2018-09-04 10:34:30 +0800

    feat: update dev-server demo

--- dev-server/src/index.js
+++ dev-server/src/index.js
@@ -1,11 +1,25 @@
 import './style.css';
+import printMe from './print.js';
 
 function component() {
   let element = document.createElement('h1');
+  let btn = document.createElement('button');
 
   element.innerHTML = 'Hello webpack';
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
 
 document.body.appendChild(component());
+
+// if (module.hot) {
+//   module.hot.accept('./print.js', () => {
+//     console.log('Accepting the updated printMe module!')
+//     printMe()
+//   })
+// }
