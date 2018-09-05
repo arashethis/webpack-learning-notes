7c5605cb169570e3f02c61f0a5b7ceac54a244b6
arashethis <arashethis@outlook.com>
2018-09-05 14:55:00 +0800

    feat: update lazy-loading demo

--- lazy-loading/src/index.js
+++ lazy-loading/src/index.js
@@ -1,11 +1,19 @@
 var btn = document.createElement('button')
 btn.innerHTML = 'Click Me!';
 
+// btn.onclick = function () {
+//   import(/* webpackChunkName: "lodash" */ 'lodash')
+//   .then(({ default: _ }) => {
+//     var element = document.createElement('div');
+//     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+//     document.body.appendChild(element);
+//   })
+// }
+
 btn.onclick = function () {
-  import(/* webpackChunkName: "lodash" */ 'lodash')
-  .then(({ default: _ }) => {
+  import(/* webpackChunkName: "utils" */ './utils.js').then(({add}) => {
     var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+    element.innerHTML = `10 + 20 = ${add(10, 20)}`;
     document.body.appendChild(element);
   })
 }
